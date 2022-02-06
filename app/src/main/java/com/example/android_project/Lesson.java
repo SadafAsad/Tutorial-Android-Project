7a4c81cbdaa762ca897b6f2b190e2e118bb34e1b
SadafAsad <sadaf98x@gmail.com>
2022-02-06 18:55:38 -0500

    Lesson class modified

--- app/src/main/java/com/example/android_project/Lesson.java
+++ app/src/main/java/com/example/android_project/Lesson.java
@@ -5,6 +5,8 @@ public class Lesson {
     private String name;
     private int length;
     private boolean status;
+    private String description;
+    private String notes;
 
     public Lesson(int number, String name, int length){
         this.number = number;
@@ -17,6 +19,9 @@ public class Lesson {
     public String getName(){ return this.name; }
     public int getLength(){ return this.length; }
     public boolean getStatus(){ return this.status; }
+    public String getDescription(){ return this.description; }
+    public String getNotes(){ return this.notes; }
 
     public void setStatus(){ this.status = true; }
+    public void setNotes(String notes){ this.notes = notes; }
 }
