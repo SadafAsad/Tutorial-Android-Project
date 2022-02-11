8816172839d9eac812c4a8fa2c2a0f6e82668b41
Samarth Joshi <samarthjoshi99@gmail.com>
2022-02-10 19:47:05 -0500

    url works now

--- app/src/main/java/com/example/android_project/ListOfLessons.java
+++ app/src/main/java/com/example/android_project/ListOfLessons.java
@@ -15,13 +15,23 @@ public class ListOfLessons {
 
     public Lesson[] lessonsList(){
         Lesson lesson1 = new Lesson(1, "Introduction to the Course", 12,
-                                    "Description for first Description for first Description for first Description for first Description for first Description for first Description for first Description for first Description for first", "");
+                                    "In this course, you will learn about Javascript, programming in js, and various applications of it. It is a scripting language, which can be added to our HTML Code using script tag, or by creating a separate file and adding it's location directory.",
+                                    "http://www.youtube.com/watch?v=1HakS7KsbCk");
         Lesson lesson2 = new Lesson(2, "What is Javascript?", 30,
-                                    "Description for second Description for second Description for second Description for second Description for second Description for second Description for second Description for second Description for second", "");
+                                    "JavaScript is a lightweight, cross-platform, and interpreted scripting language. It is well-known for the development of web pages, many non-browser environments also use it. JavaScript can be used for Client-side developments as well as Server-side developments.",
+                                    "http://www.youtube.com/watch?v=upDLs1sn7g4");
         Lesson lesson3 = new Lesson(3, "Variables and Conditionals", 80,
-                                    "", "");
+                                    "In this module, you will learn about variables and conditionals in Javascript. Variables are important because they allow you to programmatically store data. Conditionals are useful for making choices.",
+                                    "http://www.youtube.com/watch?v=qK3yEwXqaAc");
         Lesson lesson4 = new Lesson(4, "Loops", 38,
-                                    "", "");
+                                    "Loops are handy, if you want to run the same code over and over again, each time with a different value. \n JavaScript supports different kinds of loops:\n" +
+                                            "\n" +
+                                            "for - loops through a block of code a number of times\n" +
+                                            "for/in - loops through the properties of an object\n" +
+                                            "for/of - loops through the values of an iterable object\n" +
+                                            "while - loops through a block of code while a specified condition is true\n" +
+                                            "do/while - also loops through a block of code while a specified condition is true",
+                                    "http://www.youtube.com/watch?v=s9wW2PpJsmQ");
         Lesson[] lessons = new Lesson[]{lesson1, lesson2, lesson3, lesson4};
         return lessons;
     }
--- app/src/main/java/com/example/android_project/Screen3.java
+++ app/src/main/java/com/example/android_project/Screen3.java
@@ -1,13 +1,20 @@
 package com.example.android_project;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.android_project.databinding.ActivityMainBinding;
 import com.example.android_project.databinding.LessonDetailsScreenBinding;
+
+import java.util.List;
 
 public class Screen3 extends AppCompatActivity {
 
@@ -18,11 +25,40 @@ public class Screen3 extends AppCompatActivity {
         binding = LessonDetailsScreenBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
         Intent intentFromPrevScreen = getIntent();
         Lesson lesson =(Lesson) intentFromPrevScreen.getSerializableExtra("Lesson_Object");
             binding.lessonNumberAndName.setText(lesson.getNumber() + ". " + lesson.getName());
             binding.lessonLength.setText("Length: " + lesson.getLength() + " min");
             binding.lessonDescription.setText(lesson.getDescription());
+
+
+        binding.watchLesson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(lesson.getUrl()));
+                intent.setPackage("com.google.android.youtube");
+                PackageManager manager = getPackageManager();
+                List<ResolveInfo> info = manager.queryIntentActivities(intent, 0);
+                if (info.size() > 0) {
+                    startActivity(intent);
+                } else {
+                    //No Application can handle your intent
+                }
+
+
+
+
+//                Uri uri = Uri.parse(lesson.getUrl()); // missing 'http://' will cause crashed
+//                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+//                intent.setComponent(new ComponentName("com.google.android.youtube","com.google.android.youtube.PlayerActivity"));
+//                startActivity(intent);
+            }
+        });
+
     }
+
 
 }
