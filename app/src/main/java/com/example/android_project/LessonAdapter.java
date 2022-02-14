37ed0c551bd9f5104636d5cf90152c8e6595f039
SadafAsad <sadaf98x@gmail.com>
2022-02-14 17:45:32 -0500

    layouts updated

--- app/src/main/java/com/example/android_project/LessonAdapter.java
+++ app/src/main/java/com/example/android_project/LessonAdapter.java
@@ -1,25 +1,12 @@
 package com.example.android_project;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
 
 public class LessonAdapter extends BaseAdapter {
     private Context context;
@@ -49,25 +36,16 @@ public class LessonAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
+        view = inflater.inflate(R.layout.lesson_card, null);
+        TextView lesson_number = (TextView) view.findViewById(R.id.lesson_number);
+        TextView lesson_name = (TextView) view.findViewById(R.id.lesson_name);
+        TextView lesson_length = (TextView) view.findViewById(R.id.lesson_length);
+        lesson_number.setText(this.lessons[i].getNumber() + "");
+        lesson_name.setText(this.lessons[i].getName());
+        lesson_length.setText("Length: " + lessons[i].getLength() + " min");
         if (this.lessons[i].getStatus()){
-            view = inflater.inflate(R.layout.lesson_card_finished, null);
-            TextView lesson_number = (TextView) view.findViewById(R.id.lesson_number);
-            TextView lesson_name = (TextView) view.findViewById(R.id.lesson_name);
-            TextView lesson_length = (TextView) view.findViewById(R.id.lesson_length);
             ImageView finished = (ImageView) view.findViewById(R.id.lesson_status);
-            lesson_number.setText(this.lessons[i].getNumber() + "");
-            lesson_name.setText(this.lessons[i].getName());
-            lesson_length.setText("Length: " + lessons[i].getLength() + " min");
-            finished.setImageResource(R.drawable.lesson_finished);
-        }
-        else {
-            view = inflater.inflate(R.layout.lesson_card_not_finished, null);
-            TextView lesson_number = (TextView) view.findViewById(R.id.lesson_number);
-            TextView lesson_name = (TextView) view.findViewById(R.id.lesson_name);
-            TextView lesson_length = (TextView) view.findViewById(R.id.lesson_length);
-            lesson_number.setText(this.lessons[i].getNumber() + "");
-            lesson_name.setText(this.lessons[i].getName());
-            lesson_length.setText("Length: " + lessons[i].getLength() + " min");
+            finished.setVisibility(View.VISIBLE);
         }
         return view;
     }
